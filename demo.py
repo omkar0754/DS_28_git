69cb5f8ab936eec5b5e63b1802bede136c63d39e
omkar <om99shinde@gmail.com>
2026-08-12 09:06:46 +0530

    Modified both the files

--- demo.py
+++ demo.py
@@ -1,1 +1,4 @@
 print("Hello and welcome to the demo! and will be a great experience for you!")
+
+print("This is how we learn git and how we can use it in our projects and how we can collaborate with other developers and how we can manage our codebase effectively and efficiently!")
+
--- git_learning.py
+++ git_learning.py
@@ -1,1 +1,3 @@
 print("THis is how we learn git and how we can use it in our projects and how we can collaborate with other developers and how we can manage our codebase effectively and efficiently!")
+
+print("Hello and welcome to the demo! and will be a great experience for you!")
