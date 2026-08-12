11c21f7c54dd2fd175be6a9ec0b928149a8e984c
omkar <om99shinde@gmail.com>
2026-08-12 09:04:24 +0530

    Added a new file git_learning in my codebase

---
+++ git_learning.py
@@ -0,0 +1,1 @@
+print("THis is how we learn git and how we can use it in our projects and how we can collaborate with other developers and how we can manage our codebase effectively and efficiently!")
