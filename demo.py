d7ad29ad6e87b670a1dc1300df074203ca19bd02
omkar <om99shinde@gmail.com>
2026-08-13 09:09:09 +0530

    createad dev branch

--- demo.py
+++ demo.py
@@ -2,3 +2,4 @@ print("Hello and welcome to the demo! and will be a great experience for you!")
 
 print("This is how we learn git and how we can use it in our projects and how we can collaborate with other developers and how we can manage our codebase effectively and efficiently!")
 
+print("This is dev branch and we are learning how to use git and how to manage our codebase effectively and efficiently!")
