2b9db70fd496c5d99db0d5bbb8790b62cd142475
omkar <om99shinde@gmail.com>
2026-08-14 08:46:00 +0530

    Added a new line in demo.py

--- demo.py
+++ demo.py
@@ -4,3 +4,6 @@ print("This is how we learn git and how we can use it in our projects and how we
 
 print("This is dev branch and we are learning how to use git and how to manage our codebase effectively and efficiently!")
 print("This change is now done on feature branch and now we will merge it to the main branch and then we will see how we can use git in our projects and how we can collaborate with other developers and how we can manage our codebase effectively and efficiently!")
+
+
+print("This is the final change that we are making on the main branch and now we will see how we can use git in our projects and how we can collaborate with other developers and how we can manage our codebase effectively and efficiently!")
